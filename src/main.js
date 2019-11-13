2acb895ef21d1832229aa99dae9cdb0083eaffaf
Aaron C <aaron.coox@outlook.com>
2019-11-13 19:54:30 +0900

    still trying to get analytics working :'(

--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-import VueAnalytics from "vue-analytics";
-
-const isProd = process.env.NODE_ENV === "production";
-Vue.use(VueAnalytics, {
-  id: "UA-49867195-2",
-  debug: {
-    enabled: !isProd,
-    sendHitTask: isProd
-  }
-});
 
 Vue.config.productionTip = false;
 
