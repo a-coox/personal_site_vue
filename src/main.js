1f9b1d98da8800b267388f9a7698b8901fa80874
Aaron C <aaron.coox@outlook.com>
2019-11-13 12:57:50 +0900

    -

--- src/main.js
+++ src/main.js
@@ -2,18 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueAnalytics from "vue-analytics";
 
+const isProd = process.env.NODE_ENV === "production";
+Vue.use(VueAnalytics, {
+  id: "UA-49867195-2",
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd
+  }
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
 }).$mount("#app");
-
-Vue.use(VueAnalytics, {
-  id: "UA-49867195-2" //,
-  // debug: {
-  //   enabled: process.env.NODE_ENV !== "production"
-  // },
-  // autoTracking: {
-  //   exception: true
-  // }
-});
